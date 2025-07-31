fd90352fe0c265fdca7fd8531fbedbb206fb4899
Adesina Nurudeen <nubasuyu@gmail.com>
2025-07-31 03:28:17 +0100

    ammendment to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import MovieDetails from "./components/MovieDetails";
 
 function App() {
   return (
+    <>    <h1 className="text-3xl font-bold mb-6 text-center">🎬 Nurudeen Movie App</h1>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/movie/:title" element={<MovieDetails />} />
     </Routes>
+    </>
+
   );
 }
 
